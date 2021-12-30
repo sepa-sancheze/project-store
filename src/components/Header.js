51c342b6a14d0f8794b081a39d27e58971c77f1b
José Sánchez <jose.venados6@gmail.com>
2021-12-29 18:53:45 -0600

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,70 @@
-import React from 'react';
+// Header obtenido de https://tailwindui.com/preview#component-10058606cac5398d7fa2c73b64089874
+import React, { Fragment } from 'react';
+import { Disclosure } from '@headlessui/react';
+import { ShoppingCartIcon } from '@heroicons/react/outline';
+
+const navigation = [
+    { name: 'Products', href: '#', current: true },
+    { name: '', href: '#', current: false },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 function Header() {
     return (
-        <React.Fragment>
-            <h1>
-                Header
-            </h1>
-        </React.Fragment>
+        <div className="min-h-full">
+            <Disclosure as="nav" className="bg-gray-800">
+                {({ open }) => (
+                    <>
+                        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                            <div className="flex items-center justify-between h-16">
+                                <div className="flex items-center">
+                                    <div className="flex-shrink-0">
+                                        <img
+                                            className="h-8 w-8"
+                                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                            alt="Workflow"
+                                        />
+                                    </div>
+                                    <div className="hidden md:block">
+                                        <div className="ml-10 flex items-baseline space-x-4">
+                                            {navigation.map((item) => (
+                                                <a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    className={classNames(
+                                                        item.current
+                                                            ? 'bg-gray-900 text-white'
+                                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                        'px-3 py-2 rounded-md text-sm font-medium'
+                                                    )}
+                                                    aria-current={item.current ? 'page' : undefined}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="hidden md:block">
+                                    <div className="ml-4 flex items-center md:ml-6">
+                                        <button
+                                            type="button"
+                                            className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                                        >
+                                            <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </>
+                )}
+            </Disclosure>
+        </div>
     );
 }
 
