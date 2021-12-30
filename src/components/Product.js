b295824ad1531aa40f264a6871fdf80c994a9dae
José Sánchez <jose.venados6@gmail.com>
2021-12-29 18:54:27 -0600

    Update products layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,9 +6,12 @@ function Product(props) {
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
             <img className="w-full" src={props.url} alt="Sunset in the mountains"/>
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                <div className="font-bold text-xl mb-2">{ props.title }</div>
+                <p className="text-gray-700 text-base mb-2">
+                    { props.description }
+                </p>
+                <p className='font-bold text-xl'>
+                    Q. { props.price } 
                 </p>
             </div>
         </div>
--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -4,8 +4,12 @@ import { Product } from './Product';
 function ProductContainer() {
     return (
         <React.Fragment>
-            <div className="grid grid-cols-3 gap-4 content-start">
-                <Product url='img/card-top.jpg'/>
+            <div className="grid grid-cols-3 gap-4 content-start max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <Product 
+                    url='img/card-top.jpg'
+                    description='Descripción producto A'
+                    title='Producto A'
+                    price='25.00' />
             </div>
         </React.Fragment>
     );
